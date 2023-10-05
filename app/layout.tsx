4a83ce156f666d40676d9a995e092f52d5a3d42f
Serikbayev, Serik (EPAM Systems) <serik.serikbayev@astrazeneca.com>
2023-10-05 17:24:41 +0600

    add searchbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,20 @@
-import { Footer, Navbar } from '@/components';
-import './globals.css'
+import { Footer, Navbar } from "@/components";
+import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-	title: "Car hub",
+	title: "Car Hub",
 	description: "Discover the best cars in the world."
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
 		<html lang="en">
-			<Navbar />
-			<body className="relative">{children}</body>
-			<Footer />
+			<body className="relative">
+				<Navbar />
+				{children}
+				<Footer />
+			</body>
 		</html>
 	);
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,6 +26,17 @@ const Footer = () => {
 					))}
 				</div>
 			</div>
+			<div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+				<p>@2023 CarHub. All Rights Reserved</p>
+				<div className="footer__copyrights-link">
+					<Link href="/" className="text-gray-150 ">
+						Privacy Policy
+					</Link>
+					<Link href="/" className="text-gray-150 ">
+						Terms of Use
+					</Link>
+				</div>
+			</div>
 		</footer>
 	);
 };
