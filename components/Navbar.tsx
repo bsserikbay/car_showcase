d0473b58aa349a78f6909f53d6ddc32cf72d3861
Serikbayev, Serik (EPAM Systems) <serik.serikbayev@astrazeneca.com>
2023-10-12 22:27:31 +0600

    add CarCard

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,9 +6,9 @@ import { CustomButton } from ".";
 const Navbar = () => {
 	return (
 		<header className="w-full absolute z-10">
-			<nav className="max-w-[1440px] msx-auto flex justify-between items-center sm:px-16 px-6 py-4">
+			<nav className="max-w-[1440px] msx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent">
 				<Link href={"/"} className="flex justify-center items-center">
-					<Image src="/logo.svg" alt="Car Hub Logo" width={118} height={18} className="object-contain" />
+					<Image src="/logo.svg" alt="logo" width={118} height={18} className="object-contain" />
 				</Link>
 				<CustomButton title="Sign in" btnType="button" containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]" />
 			</nav>
